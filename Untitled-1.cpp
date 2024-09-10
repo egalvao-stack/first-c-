4576e415068c73e22085b23f578430cc362b1cbd
egalvao-stack <eduardo.galvao@mcvts.org>
2024-09-10 10:45:35 -0400

    Add files via upload

---
+++ Untitled-1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string name;
+    int age;
+    cout << "Hello World!" << endl;
+    cout << "Welcome to C++" << endl;
+    cout << "What is your name" << endl;
+    cin >> name;
+    cout << "Hello " << name << endl;
+    cout << "How old are you?" << endl;
+    return 0;
+}
